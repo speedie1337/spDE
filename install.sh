0667dbdc489b9e550ff345558d0a9fa29a7c2bd2
speedie <71722170+speediegamer@users.noreply.github.com>
2022-03-26 10:57:17 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
+#            ____  _____ #
+#  ___ _ __ |  _ \| ____|#
+# / __| '_ \| | | |  _|  #
+# \__ \ |_) | |_| | |___ #
+# |___/ .__/|____/|_____|#
+#     |_|                #
+# spDE // installation script bu speedie
+# Licensed under MIT.
 
 echo "Detecting shell" && echo "Found $SHELL" && echo "#!$SHELL" > install-spde.sh
-
 cat install-base.sh >> install-spde.sh && echo "Added base"
-
 chmod +x install-spde.sh && echo "Changed permission of script"
-
 ./install-spde.sh
